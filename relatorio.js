2ef5d96bbf20aa630d2bc1ad750c3fb85e8d5530
Obake18 <coimbrajuniorm>
2024-01-12 21:57:42 -0300

    Nova tentativa, uhul

--- relatorio.js
+++ relatorio.js
@@ -12,24 +12,27 @@ document.getElementById('minutos').addEventListener('change', function () {
         this.value = minutos;
     }
 });
+// Capturar os valores dos campos
+var nome = document.getElementById('nome').value;
+var mes = document.getElementById('mes').value;
+var participou = document.getElementById('atividade-sim').checked;
+var estudoBiblico = parseInt(document.getElementById('estudo-number').value);
+var horas = parseInt(document.getElementById('horas').value);
+var minutos = parseInt(document.getElementById('minutos').value);
+var observacoes = document.getElementById('observacoes').value;
 
-document.getElementById('relatorio-form').addEventListener('submit', function (event) {
-    // Capturar os valores dos campos
-    var nome = document.getElementById('nome').value;
-    var mes = document.getElementById('mes').value;
-    var participou = document.getElementById('atividade-sim').checked;
-    var estudoBiblico = parseInt(document.getElementById('estudo-number').value);
-    var horas = parseInt(document.getElementById('horas').value);
-    var minutos = parseInt(document.getElementById('minutos').value);
-    var observacoes = document.getElementById('observacoes').value;
+// Criar uma instância da classe Publicador
+var publicador = new Publicador(nome, mes, participou, estudoBiblico, horas, minutos, observacoes);
 
-    // Criar uma instância da classe Publicador
-    var publicador = new Publicador(nome, mes, participou, estudoBiblico, horas, minutos, observacoes);
+// Adicionar lógica adicional conforme necessário
+console.log(publicador);
 
-    // Adicionar lógica adicional conforme necessário
-    console.log(publicador);
-
-    // Você pode adicionar aqui a lógica para enviar o objeto para o servidor ou planilha
-
-    event.preventDefault(); // Isso evita o envio padrão do formulário
+// Enviar os dados para o Google Apps Script
+fetch('https://script.google.com/macros/s/AKfycbxkALlhFhs_fT_bmfnlNnKBZci_rTAPZM50_ziWfZ9Q0m3_q1Ijqmpdn9teBlflpT42/exec', {
+    method: 'POST',
+    mode:'no-cors',
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: `nome=${nome}&mes=${mes}&participou=${participou}&estudoBiblico=${estudoBiblico}&horas=${horas}&minutos=${minutos}&observacoes=${observacoes}`
 });
