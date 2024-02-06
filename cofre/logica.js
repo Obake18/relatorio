9636033308e0053eb80bbe6695cad2da70c259cb
Obake18 <coimbrajuniorm@gmail.com>
2024-02-06 17:57:58 -0300

    a

--- cofre/logica.js
+++ cofre/logica.js
@@ -154,3 +154,17 @@ onValue(relatoriosQuery, (snapshot) => {
     console.error(error);
 });
 
+
+
+var toggleButton = document.getElementById('mode-toggle');
+toggleButton.addEventListener('click', function () {
+    if (document.body.classList.contains('light-mode')) {
+        document.body.classList.remove('light-mode');
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+        document.body.classList.add('light-mode');
+    }
+});
+
+
